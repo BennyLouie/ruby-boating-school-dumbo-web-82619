87290c7a62ed9774ebd2c536c08fd591029ef802
github username <github email address>
2019-09-02 11:19:22 -0400

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,43 @@
-class Instructor
+require 'pry'
 
+class Instructor
+    attr_reader(:name)
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def pass_student(student, test_name)
+        find_test = []
+        find_test = BoatingTest.all.select do |test|
+            test.student == student && test.test_name == test_name
+        end
+        if find_test.size > 0
+            find_test[0].test_status = "passed"
+            find_test[0]
+        else
+            BoatingTest.new(student, test_name, "passed", self)
+        end
+    end
+
+    def fail_student(student, test_name)
+        find_test = []
+        find_test = BoatingTest.all.select do |test|
+            test.student == student && test.test_name == test_name
+        end
+        if find_test.size > 0
+            find_test[0].test_status = "failed"
+            find_test[0]
+        else
+            BoatingTest.new(student, test_name, "failed", self)
+        end
+    end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,43 @@
 class Student
+    attr_reader(:name)
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_boating_test(test_name, test_status, instructor)
+        BoatingTest.new(self, test_name, test_status, instructor)
+    end
+
+    def self.find_student(name)
+        self.all.find do |student|
+            student.name == name
+        end
+    end
+
+    def tests_taken
+        BoatingTest.all.select do |test|
+            test.student == self
+        end
+    end
+
+    def tests_passed
+        self.tests_taken.select do |test|
+            test.test_status == "passed"
+        end
+    end
+
+    def grade_percentage
+        num_tests = self.tests_taken.size
+        num_passed = self.tests_passed.size
+        num_passed.to_f / num_tests
+    end
 end
